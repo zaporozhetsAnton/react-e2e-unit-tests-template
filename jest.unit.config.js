d9efd6c8e5036aaee6aae2daa10a09017a897875
zaporozhetsAnton <zaporozhetsAnton@users.noreply.github.com>
2020-11-30 16:49:49 +0200

    Update jest.unit.config.js

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,4 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
     preset: 'ts-jest',
+    modulePaths: ['<rootDir>'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' } ),
     testPathIgnorePatterns: ['<rootDir>/e2e'],
 };
