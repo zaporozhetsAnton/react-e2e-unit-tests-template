97f5a14b0349546f05bd4d6715b41a1471ef150a
Anton Zaporozhets <zaporozhetsanton@gmail.com>
2021-03-15 08:52:11 +0200

    ciPuppeteerTestsRun function refactoring

--- e2e/config/ciPuppeteerTestsRun.js
+++ e2e/config/ciPuppeteerTestsRun.js
@@ -3,13 +3,9 @@ const { execSync } = require('child_process');
 // Puppeteer fails time to time during CI checks
 // ciPuppeteerTestsRun function will rerun e2e tests while they succeed
 
-const ciPuppeteerTestsRun = (max = 1) => {
-    let runTestsCommand;
-    if (max > 1) {
-        runTestsCommand = new Array(max).fill(process.env.COMMAND).join(' || ');
-    } else {
-        runTestsCommand = process.env.COMMAND;
-    }
+const ciPuppeteerTestsRun = (maxAttempts = 1) => {
+    const runTestsCommand =
+        maxAttempts > 1 ? new Array(maxAttempts).fill(process.env.COMMAND).join(' || ') : process.env.COMMAND;
     execSync(runTestsCommand);
 };
 
