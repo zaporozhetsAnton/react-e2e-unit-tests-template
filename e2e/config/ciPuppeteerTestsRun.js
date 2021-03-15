c9108c3bdce978bfa36dc69de8fb6b2ae56e03d5
Anton Zaporozhets <zaporozhetsanton@gmail.com>
2021-03-15 08:48:39 +0200

    ciPuppeteerTestsRun function refactoring

--- e2e/config/ciPuppeteerTestsRun.js
+++ e2e/config/ciPuppeteerTestsRun.js
@@ -4,14 +4,13 @@ const { execSync } = require('child_process');
 // ciPuppeteerTestsRun function will rerun e2e tests while they succeed
 
 const ciPuppeteerTestsRun = (max = 1) => {
-    const COMMAND = process.argv.find((val) => val.includes('COMMAND')).replace('COMMAND=', '');
     let runTestsCommand;
     if (max > 1) {
-        runTestsCommand = new Array(max).fill(COMMAND).join(' || ');
+        runTestsCommand = new Array(max).fill(process.env.COMMAND).join(' || ');
     } else {
-        runTestsCommand = COMMAND;
+        runTestsCommand = process.env.COMMAND;
     }
     execSync(runTestsCommand);
 };
 
-ciPuppeteerTestsRun(10);
+ciPuppeteerTestsRun(5);
